dea3e27a314a50abdc532e748837f1e9de7de1d3
Jake Garlick <jacobgarlick@msn.com>
2019-09-10 13:22:45 -0600

    added initial content to html, small styling updates, images, and initial script

---
+++ assets/javascript/game.js
@@ -0,0 +1,39 @@
+// Combo of Calc, Letter Guess, 
+
+// Global Variables
+var targetScore = 0;
+var wins = 0;
+var losses = 0;
+var roundScore = 0;
+var crystals {
+    crystal {
+        value = [];
+        name = ["Green", "Blue", "Red" , "Gold"];
+    }};
+
+// Functions
+function generateRandomNumber() {
+
+};
+
+function gameStart() {
+
+};
+
+function gameReset() {
+
+};
+
+function addToRoundScore() {
+
+};
+
+function checkIfWon() {
+
+};
+
+
+// jQuery Click Events
+$("#crystals").on("click", function () {
+
+});
