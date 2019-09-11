ffe405917496aa8c02734c3cf663b7a664e80450
Jake Garlick <jacobgarlick@msn.com>
2019-09-11 10:01:25 -0600

    updated html to be manipulated by JS, updated JS to add random values to each crystal

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,65 @@
 // Combo of Calc, Letter Guess, 
 
 // Global Variables
-var targetScore = 0;
+var targetScore = 53;
 var wins = 0;
 var losses = 0;
 var roundScore = 0;
-var crystals {
-    crystal {
-        value = [];
-        name = ["Green", "Blue", "Red" , "Gold"];
-    }};
+var numberOptions = [1, 2, 3, 4 , 5, 6];
+
+$("#number-to-guess").text(targetScore)
 
 // Functions
+for (var i = 0; i < numberOptions.length; i++) {
+    var randomNumber = Math.floor(Math.random() * 2);
+    $("#crystal-color1").attr("data-crystalvalue", numberOptions[i] + randomNumber);
+};
+
+for (var i = 0; i < numberOptions.length; i++) {
+    var randomNumber = Math.floor(Math.random() * 2);
+    $("#crystal-color2").attr("data-crystalvalue", numberOptions[i] + randomNumber);
+};
+
+for (var i = 0; i < numberOptions.length; i++) {
+    var randomNumber = Math.floor(Math.random() * 2);
+    $("#crystal-color3").attr("data-crystalvalue", numberOptions[i] + randomNumber);
+};
+
+for (var i = 0; i < numberOptions.length; i++) {
+    var randomNumber = Math.floor(Math.random() * 2);
+    $("#crystal-color4").attr("data-crystalvalue", numberOptions[i] + randomNumber);
+};
+
+$("#crystal-color1").on("click", function() {
+    var valueCrystal = ($(this).attr("data-crystalvalue"));
+    valueCrystal = parseInt(valueCrystal);
+    roundScore += valueCrystal;
+    console.log("value crystal " + valueCrystal)
+});
+
+$("#crystal-color2").on("click", function() {
+    var valueCrystal = ($(this).attr("data-crystalvalue"));
+    valueCrystal = parseInt(valueCrystal);
+    roundScore += valueCrystal;
+    console.log("value crystal " + valueCrystal)
+});
+
+$("#crystal-color3").on("click", function() {
+    var valueCrystal = ($(this).attr("data-crystalvalue"));
+    valueCrystal = parseInt(valueCrystal);
+    roundScore += valueCrystal;
+    console.log("value crystal " + valueCrystal)
+});
+
+$("#crystal-color4").on("click", function() {
+    var valueCrystal = ($(this).attr("data-crystalvalue"));
+    valueCrystal = parseInt(valueCrystal);
+    roundScore += valueCrystal;
+    console.log("value crystal " + valueCrystal)
+});
+
+
+
 function generateRandomNumber() {
 
 };
