6e83bde63c2a3411550eab78fdea3a97349ca798
Jake Garlick <jacobgarlick@msn.com>
2019-09-12 09:39:17 -0600

    updated some javascript for better user experience per value of crystals

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,42 +4,36 @@ var wins = 0;
 var losses = 0;
 var roundScore = 0;
 
-
+// Inital Crystal Values are 0
 var crystalNumber = 0;
 var crystalNumber2 = 0;
 var crystalNumber3 = 0;
 var crystalNumber4 = 0;
 
 var scoreTarget = $("#number-to-guess").text(targetScore)
-// console.log(crystalNumber);
+
 // Functions
-
-// $("data-crystalvalue").attr(crystalNumber);
-
+// Generates new random number each game, to try to keep wide range of potential values, each crystalNumber is calculated at a progressive value
 function generateRandomNumber() {
-    crystalNumber = (Math.floor(Math.random() * 11) + 1);
-    crystalNumber2 = (Math.floor(Math.random() * 11) + 1);
-    crystalNumber3 = (Math.floor(Math.random() * 11) + 1);
+    crystalNumber = (Math.floor(Math.random() * 4) + 1);
+    crystalNumber2 = (Math.floor(Math.random() * 7) + 1);
+    crystalNumber3 = (Math.floor(Math.random() * 19) + 1);
     crystalNumber4 = (Math.floor(Math.random() * 11) + 1);
 };
-
+// Set paramaters for new game
 function gameStart() {
     targetScore = (Math.floor(Math.random() * 100) + 21);
     $("#number-to-guess").text(targetScore);
     generateRandomNumber();
 };
-
+// Set game reset, function run at end of every game
 function gameReset() {
     gameStart();
     generateRandomNumber();
     roundScore = 0;
     $("#round-score").text(roundScore);
 };
-
-// function addToRoundScore() {
-
-// };
-
+// Check to see if player won based on score, called at bottom
 function checkIfWon() {
     if (roundScore === targetScore) {
         wins++;
@@ -56,6 +50,7 @@ function checkIfWon() {
 
 
 // jQuery Click Events
+// Each crystal color has a click event to establish value and add to variable plus rewrite HTML, values are console logged out as well for reference
 $("#crystal-color1").on("click", function () {
     var valueCrystal = ($(this).attr("data-crystalvalue"));
     valueCrystal = parseInt(crystalNumber);
@@ -93,7 +88,7 @@ $("#crystal-color4").on("click", function () {
 });
 
 // Calls
-
+// Run game and see if player has won
 gameStart();
 
 checkIfWon();
