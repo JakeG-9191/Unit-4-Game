d266d884175d0200596b99c580100b1d2fe37d09
Jake Garlick <jacobgarlick@msn.com>
2019-09-11 19:34:42 -0600

    added random value variables and function for crystals

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,72 +1,30 @@
 // Combo of Calc, Letter Guess, 
 
 // Global Variables
-var targetScore = 53;
+var targetScore = 0;
 var wins = 0;
 var losses = 0;
 var roundScore = 0;
-var numberOptions = [12, 4, 1, 7, 3, 9];
+var crystalNumber = (Math.floor(Math.random() * 12) + 1);
 
-$("#number-to-guess").text(targetScore)
+var scoreTarget = $("#number-to-guess").text(targetScore)
 
 // Functions
-for (var i = 0; i < numberOptions.length; i++) {
-    var randomNumber = Math.floor(Math.random() * 2);
-    $("#crystal-color1").attr("data-crystalvalue", numberOptions[i] + randomNumber);
+
+for (var i = 0; i < 4; i++) {
+   
 };
 
-for (var i = 0; i < numberOptions.length; i++) {
-    var randomNumber = Math.floor(Math.random() * 2);
-    $("#crystal-color2").attr("data-crystalvalue", numberOptions[i] + randomNumber);
-};
-
-for (var i = 0; i < numberOptions.length; i++) {
-    var randomNumber = Math.floor(Math.random() * 2);
-    $("#crystal-color3").attr("data-crystalvalue", numberOptions[i] + randomNumber);
-};
-
-for (var i = 0; i < numberOptions.length; i++) {
-    var randomNumber = Math.floor(Math.random() * 2);
-    $("#crystal-color4").attr("data-crystalvalue", numberOptions[i] + randomNumber);
-};
-
-$("#crystal-color1").on("click", function() {
-    var valueCrystal = ($(this).attr("data-crystalvalue"));
-    valueCrystal = parseInt(valueCrystal);
-    roundScore += valueCrystal;
-    console.log("value crystal " + valueCrystal)
-});
-
-$("#crystal-color2").on("click", function() {
-    var valueCrystal = ($(this).attr("data-crystalvalue"));
-    valueCrystal = parseInt(valueCrystal);
-    roundScore += valueCrystal;
-    console.log("value crystal " + valueCrystal)
-});
-
-$("#crystal-color3").on("click", function() {
-    var valueCrystal = ($(this).attr("data-crystalvalue"));
-    valueCrystal = parseInt(valueCrystal);
-    roundScore += valueCrystal;
-    console.log("value crystal " + valueCrystal)
-});
-
-$("#crystal-color4").on("click", function() {
-    var valueCrystal = ($(this).attr("data-crystalvalue"));
-    valueCrystal = parseInt(valueCrystal);
-    roundScore += valueCrystal;
-    console.log("value crystal " + valueCrystal)
-});
-
-
+console.log(crystalNumber)
 
 function generateRandomNumber() {
 
 };
 
 function gameStart() {
-
-};
+    targetScore = (Math.floor(Math.random() * 100) + 21);
+    $("#number-to-guess").text("Your Target Score is: " + targetScore);
+ };
 
 function gameReset() {
 
@@ -82,6 +40,17 @@ function checkIfWon() {
 
 
 // jQuery Click Events
+$("#crystal-color1").on("click", function() {
+    var valueCrystal = ($(this).attr("data-crystalvalue"));
+    valueCrystal = parseInt(valueCrystal);
+    roundScore += valueCrystal;
+    console.log("value crystal " + valueCrystal)
+});
+
 $("#crystals").on("click", function () {
 
 });
+
+// Calls
+
+gameStart();
